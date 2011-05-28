2a4371a8952de259fb24d2887b383cde1e160e87
Andrew Kulakov <avk@8xx8.ru>
2011-05-28 22:46:36 +0400

    Init class interface

---
+++ GoogleTranslater.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * GoogleTranslater is PHP interface for http://translate.google.com/
+ * It send request to google translate service, get response and provide 
+ * translated text.
+ *
+ * @author Andrew Kulakov <avk@8xx8.ru>
+ * @version 1.0.0
+ * @license https://github.com/Andrew8xx8/GoogleTranslater/blob/master/MIT-LICENSE.txt
+ * @copyright Andrew Kulakov (c) 2011
+ * @licenze CI
+ */
+
+class GoogleTranslater {
+    public function _construct() {
+
+    }   
+    
+    public function translateText($text, $fromLanguage = "en", $toLanguage = "ru") {
+
+    } 
+    
+    public function translateArray($array, $fromLanguage = "en", $toLanguage = "ru") {
+
+    }
+
+    public function getLanguages(){
+
+    }
+}
+?>
